7151ceb09823b694b505e9054c357aeea3a80f9f
Sönke Liebau <soenke.liebau@stackable.de>
2021-09-13 17:39:32 +0200

    - Added script and tags in playbook to test changes locally without committing anything. - Updated README.adoc

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+ansible-playbook playbook/playbook.yaml --tags "local" --extra-vars "gh_access_token=unneeded base_dir=$(pwd) commit_hash=12345 original_commit_message='original message'"
