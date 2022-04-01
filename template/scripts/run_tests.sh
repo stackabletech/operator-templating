e1c0395e0c813be4c413615cda2b386d3165bc25
Sönke Liebau <soenke.liebau@stackable.de>
2022-04-01 14:04:10 +0200

    Remove accidentally committed test-definition.yaml , change extensions of kuttl-test.yaml.j2 so that templating doesn't get confused.

--- template/scripts/run_tests.sh
+++ template/scripts/run_tests.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/env bash
+set -e
 
 # Register absolute paths to pass to Ansible so the location of the role is irrelevant
 # for the run
 TESTDIR="$(pwd)/tests"
 WORKDIR="$(pwd)/tests/_work"
 
-# Create roles dir
+# Create dirs
 mkdir -p tests/ansible/roles
+mkdir -p $WORKDIR
 
 # Install Ansible role if needed
 pushd tests/ansible
 ansible-galaxy role install -r requirements.yaml -p ./roles
 
 # TODO: create pipenv in files for script thingy
-
-# Write test scenarios
-echo $TESTDIR
-echo $WORKDIR
-mkdir -p $WORKDIR
 
 # Funnel via JSON to ensure that values are escaped properly
 echo '{}' | jq '{work_dir: $WORKDIR, test_dir: $TESTDIR}' --arg WORKDIR $WORKDIR --arg TESTDIR $TESTDIR > ${WORKDIR}/vars.json
@@ -27,5 +24,6 @@ ansible-playbook playbook.yaml --extra-vars "@${WORKDIR}/vars.json"
 popd
 
 # Run tests
-pushd tests/test-work
+pushd tests/_work
 kubectl kuttl test
+popd
