ce9fd1df72cd252982a4f125d6d5df1dd2400492
Sönke Liebau <soenke.liebau@stackable.de>
2022-04-01 14:11:18 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- template/scripts/run_tests.sh
+++ template/scripts/run_tests.sh
@@ -8,7 +8,7 @@ WORKDIR="$(pwd)/tests/_work"
 
 # Create dirs
 mkdir -p tests/ansible/roles
-mkdir -p $WORKDIR
+mkdir -p "$WORKDIR"
 
 # Install Ansible role if needed
 pushd tests/ansible
@@ -17,7 +17,7 @@ ansible-galaxy role install -r requirements.yaml -p ./roles
 # TODO: create pipenv in files for script thingy
 
 # Funnel via JSON to ensure that values are escaped properly
-echo '{}' | jq '{work_dir: $WORKDIR, test_dir: $TESTDIR}' --arg WORKDIR $WORKDIR --arg TESTDIR $TESTDIR > ${WORKDIR}/vars.json
+echo '{}' | jq '{work_dir: $WORKDIR, test_dir: $TESTDIR}' --arg WORKDIR "$WORKDIR" --arg TESTDIR "$TESTDIR" > "${WORKDIR}"/vars.json
 
 # Run playbook to generate test scenarios
 ansible-playbook playbook.yaml --extra-vars "@${WORKDIR}/vars.json"
