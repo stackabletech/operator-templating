0bfc699799d373eb108d16ac89530af82ed35f9f
Felix Hennig <fhennig@users.noreply.github.com>
2022-08-09 15:30:53 +0200

    Added docs_templating script (#163)
    * Added docs_templating script
    
    * simplified sed expression
    
    * script improvements
    
    * Update template/scripts/docs_templating.sh
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update template/scripts/docs_templating.sh
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update template/scripts/docs_templating.sh
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ template/scripts/docs_templating.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Reads a file with variables to insert into templates, and templates all .*.j2 files
+# in the 'docs' directory.
+#
+# dependencies
+# pip install jinja2-cli
+
+docs_dir="$(dirname "$0")/../docs"
+templating_vars_file="$docs_dir/templating_vars.yaml"
+
+# Check if files need templating
+if [[ -z $(find "$docs_dir" -name '*.j2') ]];
+then
+  echo "No files need templating, exiting."
+  exit
+fi
+
+# Check if jinja2 is there
+if ! command -v jinja2 &> /dev/null
+then
+  echo "jinja2 could not be found. Use 'pip install jinja2-cli' to install it."
+  exit
+fi
+
+# Check if templating vars file exists
+if [[ ! -f "$templating_vars_file" ]];
+then
+  echo "$templating_vars_file does not exist, cannot start templating."
+fi
+
+for file in $(find "$docs_dir" -name '*.j2')
+do
+  new_file_name=$(echo "$file" | sed 's/.j2$//')  # cut of the '.j2'
+  echo "templating $new_file_name"
+  jinja2 "$file" "$templating_vars_file" -o "$new_file_name"
+done
+
+echo "done"
