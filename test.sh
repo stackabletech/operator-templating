62bc6c7fd930c899c703516607f0e19c91efd27b
Nick Larsen <nick.larsen@stackable.tech>
2024-10-29 14:15:53 +0100

    chore(playbook): auto recreate the work directory

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+rm -fr work || true
+mkdir -p work
 ansible-playbook playbook/playbook.yaml --tags "local" --extra-vars "gh_access_token=unneeded base_dir=$(pwd) commit_hash=12345 reason='original message'"
