3a907662c088b22c500934a08c570db495430223
Natalie Klestrup Röijezon <nat@nullable.se>
2025-06-11 13:10:44 +0200

    Satisfy pre-commit

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 
 rm -fr work || true
 mkdir -p work
-ansible-playbook playbook/playbook.yaml --tags "local" --extra-vars "gh_access_token=unneeded base_dir=$(pwd) commit_hash=12345 reason='original message'" "$@"
+ansible-playbook playbook/playbook.yaml --tags="local" --extra-vars "gh_access_token=unneeded base_dir=$(pwd) commit_hash=12345 reason='original message'" "$@"
